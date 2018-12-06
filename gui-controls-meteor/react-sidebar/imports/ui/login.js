e0434aff086b54d9213a9c91ef698475cbfa2658
Jane <jtian42@gmail.com>
2018-12-06 16:55:32 +1300

    Added basic login page with no validation

---
+++ gui-controls-meteor/react-sidebar/imports/ui/login.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import ReactDOM, { render } from "react-dom";
+
+import Path from "../../client/path.js";
+import "./login.css"
+
+const styles = {
+    content: {
+        padding: "54px 32px"
+    },
+    button: {
+        padding: "18px 52px"
+    }
+};
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            id: "",
+            password: ""
+        };
+    }
+    
+    validateForm() {
+        return this.state.id.length >= 7 && this.state.password.length > 0; 
+    }
+    
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+    
+    handleSubmit = event => {
+        event.preventDefault();
+        ReactDOM.render(<Path />, document.getElementById("app"));
+        localStorage.setItem("auth", "true");
+    }
+    
+    render() {
+        return (
+            <div className="Login" style={styles.content}>
+                <div className="text-center" style={{ margin: "10px 0" }}>
+                    <h4>Sign in</h4>
+                </div>
+                <hr />
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="id" bsSize="large">
+                        <ControlLabel>ID number:</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="text"
+                            placeholder="Enter ID number"
+                            value={this.state.id}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                        <ControlLabel>Password:</ControlLabel>
+                        <FormControl
+                            type="password"
+                            placeholder="Enter password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <div style={styles.button}>
+                        <Button
+                            block
+                            bsSize="large"
+                            disabled={!this.validateForm()}
+                            type="submit"
+                        >
+                        Login
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
